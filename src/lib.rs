0291c656c71ce01604c51d2734d132efc59e333d
Joe Noël <joe.noel@focusrite.com>
2022-01-21 23:58:35 +0000

    Rename nodes to dsp

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ mod audio_process;
 mod buffer;
 mod commands;
 mod context;
+mod dsp;
 mod graph;
-mod nodes;
 mod parameter;
 mod realtime;
 mod timestamp;
@@ -13,9 +13,9 @@ pub type Level = utility::level::Level;
 pub type Context = context::Context;
 pub type Timestamp = timestamp::Timestamp;
 
-pub type Gain = nodes::gain::node::GainNode;
-pub type Oscillator = nodes::oscillator::node::OscillatorNode;
-pub type Sampler = nodes::sampler::node::SamplerNode;
+pub type Gain = dsp::gain::node::GainNode;
+pub type Oscillator = dsp::oscillator::node::OscillatorNode;
+pub type Sampler = dsp::sampler::node::SamplerNode;
 
 pub type AudioBufferSlice<'a> = buffer::audio_buffer_slice::AudioBufferSlice<'a>;
 pub type OwnedAudioBuffer = buffer::owned_audio_buffer::OwnedAudioBuffer;
