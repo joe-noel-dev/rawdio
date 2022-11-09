139b6a16a96110426cb15bf25d071ca436c4401d
Joe Noël <joe.noel@focusrite.com>
2022-11-09 21:02:38 +0000

    Remove random from benchmark

--- benches/audio-buffer-benchmarks.rs
+++ benches/audio-buffer-benchmarks.rs
@@ -1,14 +1,16 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use rand::*;
 use rust_audio_engine::{AudioBuffer, OwnedAudioBuffer, SampleLocation};
 
 fn read_and_write_interleaved() {
-    let mut buffer = OwnedAudioBuffer::new(1000000, 2, 44100);
-    let mut rng = rand::thread_rng();
+    let num_frames = 1_000_000;
+    let num_channels = 2;
+    let sample_rate = 44_100;
+
+    let mut buffer = OwnedAudioBuffer::new(num_frames, num_channels, sample_rate);
 
     for frame in 0..buffer.num_frames() {
         for channel in 0..buffer.num_channels() {
-            buffer.set_sample(SampleLocation::new(channel, frame), rng.gen());
+            buffer.set_sample(SampleLocation::new(channel, frame), 0.0);
         }
     }
 
@@ -21,12 +23,15 @@ fn read_and_write_interleaved() {
 }
 
 fn read_and_write_non_interleaved() {
-    let mut buffer = OwnedAudioBuffer::new(1000000, 2, 44100);
-    let mut rng = rand::thread_rng();
+    let num_frames = 1_000_000;
+    let num_channels = 2;
+    let sample_rate = 44_100;
+
+    let mut buffer = OwnedAudioBuffer::new(num_frames, num_channels, sample_rate);
 
     for channel in 0..buffer.num_channels() {
         for frame in 0..buffer.num_frames() {
-            buffer.set_sample(SampleLocation::new(channel, frame), rng.gen());
+            buffer.set_sample(SampleLocation::new(channel, frame), 0.0);
         }
     }
 
